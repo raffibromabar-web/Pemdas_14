6885f8cc809d077867ee91acd52dda8b5fab3268
raffibromabar-web <raffibromabar@gmail.com>
2026-06-01 15:42:52 +0700

    menambahkan fungsi

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -10,3 +10,6 @@ int main()
     //membuat input untuk menangkap nama file
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
