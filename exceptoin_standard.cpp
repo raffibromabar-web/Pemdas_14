cbbdbfb72b170844b6e017900aebdd3c3bebf3fe
raffibromabar-web <raffibromabar@gmail.com>
2026-06-01 15:40:55 +0700

    menambahkan tampilan teks

--- exceptoin_standard.cpp
+++ exceptoin_standard.cpp
@@ -20,6 +20,11 @@ try
 //kondisi jika terjadi error didalam try
 
 catch (exception& e)
-
 {
+    cout << e.what() << endl;
+
+}
+cout << "Baris program terakhir" << endl; //penanda 2
+
+return 0;
 }
